88e3b2d8ba3daaf2dc92f1020e8465d62fb0ad4c
Eliezer Nascimento <elieserdariogarbin@gmail.com>
2020-07-27 14:00:42 -0300

    instalando styled components

--- devflix/src/components/Menu/index.js
+++ devflix/src/components/Menu/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import logo from '../../assets/img/logo.png';
 import './Menu.css';
+import ButtonLink from './components/ButtonLink';
 
 function Menu() {
     return (
         <nav className="Menu">
-        <a href="/">
-            <img className="Logo" src={logo} alt="Devflix logo" />
-        </a>
+            <a href="/">
+                <img className="Logo" src={logo} alt="Devflix logo" />
+            </a>
 
-        <a className="ButtonLink" href="/" >
-            Novo video
-        </a>
+            <ButtonLink className="ButtonLink" href="/" >
+                Novo video
+            </ButtonLink>
         </nav>
     );
 }
