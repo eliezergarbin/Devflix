0b9ff4edd909d04ad999b3f096eb211acc83fd0c
Eliezer Nascimento <elieserdariogarbin@gmail.com>
2020-07-28 09:46:05 -0300

    instalando router dom

--- devflix/src/index.js
+++ devflix/src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="" App />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
